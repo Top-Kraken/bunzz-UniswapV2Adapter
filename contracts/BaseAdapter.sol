be5cb035643c5ee7a2c34be7a367d93457519022
Top-Kraken <topkyle521@gmail.com>
2023-03-13 12:05:49 -0400

    Add exceptions

--- contracts/BaseAdapter.sol
+++ contracts/BaseAdapter.sol
@@ -9,6 +9,10 @@ import "./lib/Maintainable.sol";
 abstract contract BaseAdapter is Maintainable {
     using SafeERC20 for IERC20;
 
+    uint256 internal constant UINT_MAX = type(uint256).max;
+    uint256 public swapGasEstimate;
+    string public name;
+
     event BaseAdapterSwap(
         address indexed _tokenFrom,
         address indexed _tokenTo,
@@ -18,30 +22,18 @@ abstract contract BaseAdapter is Maintainable {
     event UpdatedGasEstimate(address indexed _adapter, uint256 _newEstimate);
     event Recovered(address indexed _asset, uint256 amount);
 
-    uint256 internal constant UINT_MAX = type(uint256).max;
-    uint256 public swapGasEstimate;
-    string public name;
-
     constructor(string memory _name, uint256 _gasEstimate) {
         setName(_name);
         setSwapGasEstimate(_gasEstimate);
     }
 
-    function setName(string memory _name) internal {
-        require(bytes(_name).length != 0, "Invalid adapter name");
-        name = _name;
-    }
-
-    function setSwapGasEstimate(uint256 _estimate) public onlyMaintainer {
-        require(_estimate != 0, "Invalid gas-estimate");
-        swapGasEstimate = _estimate;
-        emit UpdatedGasEstimate(address(this), _estimate);
-    }
+    receive() external payable {}
 
     function revokeAllowance(address _token, address _spender)
         external
         onlyMaintainer
     {
+        require(_token != address(0), "BaseAdapter: Invalid token address");
         IERC20(_token).safeApprove(_spender, 0);
     }
 
@@ -49,6 +41,10 @@ abstract contract BaseAdapter is Maintainable {
         external
         onlyMaintainer
     {
+        require(
+            _tokenAddress != address(0),
+            "BaseAdapter: Invalid token address"
+        );
         require(_tokenAmount > 0, "BaseAdapter: Nothing to recover");
         IERC20(_tokenAddress).safeTransfer(msg.sender, _tokenAmount);
         emit Recovered(_tokenAddress, _tokenAmount);
@@ -60,6 +56,29 @@ abstract contract BaseAdapter is Maintainable {
         emit Recovered(address(0), _amount);
     }
 
+    function swap(
+        uint256 _amountIn,
+        uint256 _amountOut,
+        address _fromToken,
+        address _toToken,
+        address _to
+    ) external {
+        require(
+            _fromToken != address(0),
+            "BaseAdapter: Invalid fromToken address"
+        );
+        require(
+            _fromToken != address(0),
+            "BaseAdapter: Invalid toToken address"
+        );
+        require(_amountIn > 0, "BaseAdapter: Invalid amountIn");
+        uint256 toBal0 = IERC20(_toToken).balanceOf(_to);
+        _swap(_amountIn, _amountOut, _fromToken, _toToken, _to);
+        uint256 diff = IERC20(_toToken).balanceOf(_to) - toBal0;
+        require(diff >= _amountOut, "Insufficient amount-out");
+        emit BaseAdapterSwap(_fromToken, _toToken, _amountIn, _amountOut);
+    }
+
     function query(
         uint256 _amountIn,
         address _tokenIn,
@@ -68,18 +87,15 @@ abstract contract BaseAdapter is Maintainable {
         return _query(_amountIn, _tokenIn, _tokenOut);
     }
 
-    function swap(
-        uint256 _amountIn,
-        uint256 _amountOut,
-        address _fromToken,
-        address _toToken,
-        address _to
-    ) external {
-        uint256 toBal0 = IERC20(_toToken).balanceOf(_to);
-        _swap(_amountIn, _amountOut, _fromToken, _toToken, _to);
-        uint256 diff = IERC20(_toToken).balanceOf(_to) - toBal0;
-        require(diff >= _amountOut, "Insufficient amount-out");
-        emit BaseAdapterSwap(_fromToken, _toToken, _amountIn, _amountOut);
+    function setSwapGasEstimate(uint256 _estimate) public onlyMaintainer {
+        require(_estimate != 0, "Invalid gas-estimate");
+        swapGasEstimate = _estimate;
+        emit UpdatedGasEstimate(address(this), _estimate);
+    }
+
+    function setName(string memory _name) internal {
+        require(bytes(_name).length != 0, "Invalid adapter name");
+        name = _name;
     }
 
     function _returnTo(
@@ -103,6 +119,4 @@ abstract contract BaseAdapter is Maintainable {
         address _tokenIn,
         address _tokenOut
     ) internal view virtual returns (uint256);
-
-    receive() external payable {}
 }
